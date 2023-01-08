8a632dc737de6b22999efee389fb9d1384f1fa84
Polina Kulikova <pomidorishna@gmail.com>
2023-01-08 21:38:34 +0300

    Задача 1 готова

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введи трёхзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+string second = n.ToString();
+Console.WriteLine(second[1]);
